83da0542d91a6f1894d3726db85423769e399cfe
Sara Brito <1231266@isep.ipp.pt>
2026-05-21 10:37:32 +0100

    Ficha 09.03 e 09.04 - Ficheiros de configuração

---
+++ private/includes/footer.php
@@ -0,0 +1,4 @@
+<!-- Bootstrap JS and custom JS -->
+ <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ private/includes/sidebar.php
@@ -0,0 +1,20 @@
+        <aside class="col-md-3 col-lg-2 bg-secondary text-white p-3 min-vh-100">
+            <h4>Menu</h4>
+            <nav>
+                <a href="../private/views/clientes/lista.html" class="nav-link text-white px-0 mb-2 d-block"> 
+                    <i class="fas fa-users me-2"></i> Clientes
+                </a>
+                <a href="../private/views/agendamento/agendamento.html"class="nav-link text-white px-0 mb-2 d-block"> 
+                    <i class="fas fa-calendar-alt me-2"></i> Agendamento de treinos
+                </a>
+                <a href="../private/views/planos/planos.html" class="nav-link text-white px-0 mb-2 d-block"> 
+                    <i class="fas fa-dumbbell me-2"></i> Planos de Treino
+                </a>
+                <a href="../private/views/equipamentos/equipamentos.html" class="nav-link text-white px-0 mb-2 d-block"> 
+                    <i class="fas fa-cogs me-2"></i> Equipamentos
+                </a>
+                <a href="../private/views/produtos-servicos/produtos_servicos.html" class="nav-link text-white px-0 mb-2 d-block"> 
+                    <i class="fas fa-box-open me-2"></i> Produtos e Serviços
+                </a>
+            </nav>
+        </aside>
